e2eb32b376585b54cfeb614f7b050708cf4b713a
Michael Brenan <blacksmithgu@gmail.com>
2021-01-26 18:54:59 -0800

    Precedence-respecting field parsing

--- src/query.ts
+++ src/query.ts
@@ -1,7 +1,20 @@
 /** Provides query parsing from plain-text. */
+import 'parsimmon';
+import * as Parsimmon from 'parsimmon';
 
 /** The supported query types (corresponding to view types). */
 export type QueryType = 'list' | 'table';
+
+export type LiteralType = 'number' | 'string' | 'duration' | 'date' | 'rating';
+export type LiteralTypeRepr<T extends LiteralType> =
+    T extends 'number' ? number :
+    T extends 'string' ? string :
+    T extends 'duration' ? number :
+    T extends 'date' ? number :
+    T extends 'rating' ? number :
+    any;
+
+export type BinaryOp = '+' | '-' | '>' | '>=' | '<=' | '<' | '=';
 
 /** A (potentially computed) field to select or compare against. */
 
@@ -10,7 +23,51 @@ export interface Field {
      * The field type - literals are actual raw values, variables are things in the frontmatter,
      * and computed are complex functions of subfields.
      * */
-    type: 'literal' | 'variable' | 'computed';
+    type: 'literal' | 'variable' | 'binaryop';
+}
+
+export interface LiteralField<T extends LiteralType> extends Field {
+    type: 'literal';
+    valueType: T;
+    value: LiteralTypeRepr<T>;
+}
+
+export interface VariableField extends Field {
+    type: 'variable';
+    name: string;
+}
+
+export interface BinaryOpField extends Field {
+    type: 'binaryop';
+    left: Field;
+    right: Field;
+    op: BinaryOp;
+}
+
+/** Fields used in the query portion. */
+export interface NamedField {
+    /** The effective name of this field. */
+    name: string;
+    /** The value of this field. */
+    field: Field;
+}
+
+export namespace Fields {
+    export function variable(name: string): VariableField {
+        return { type: 'variable', name };
+    }
+
+    export function literal<T extends LiteralType>(vtype: T, val: LiteralTypeRepr<T>): Field {
+        return { type: 'literal', valueType: vtype, value: val } as LiteralField<T>;
+    }
+    
+    export function binaryOp(left: Field, op: BinaryOp, right: Field): Field {
+        return { left, op, right } as BinaryOpField;
+    }
+
+    export function named(name: string, field: Field): NamedField {
+        return { name, field } as NamedField;
+    }
 }
 
 /** A query sort by field, for determining sort order. */
@@ -26,7 +83,7 @@ export interface Query {
     /** The view type to render this query in. */
     type: QueryType;
     /** The fields (computed or otherwise) to select. */
-    fields: Field[];
+    fields: NamedField[];
     /** The tags to select from. */
     from: string[];
     /** Tags or subtags to exclude. */
@@ -36,6 +93,75 @@ export interface Query {
     /** */
     sortBy: QuerySortBy[];
 }
+
+/** Typings for the outputs of all of the parser combinators. */
+interface QueryLanguageTypes {
+    queryType: QueryType;
+
+    number: number;
+    string: string;
+    identifier: string;
+    binaryPlusMinus: BinaryOp;
+    binaryCompareOp: BinaryOp;
+
+    // Field-related parsers.
+    variableField: VariableField;
+    numberField: Field;
+    stringField: Field;
+    atomField: Field;
+
+    binaryPlusMinusField: Field;
+    binaryOpField: Field;
+    parensField: Field;
+    field: Field;
+    explicitNamedField: NamedField;
+    namedField: NamedField;
+}
+
+/** A parsimmon-powered parser-combinator implementation of the query language. */
+export const QUERY_LANGUAGE = Parsimmon.createLanguage<QueryLanguageTypes>({
+    // Constants & words, like 'WHERE', 'SORT BY', so on.
+    queryType: q => Parsimmon.alt<string>(Parsimmon.regexp(/LIST/i), Parsimmon.regexp(/TABLE/i)).map(str => str.toLowerCase() as QueryType),
+    number: q => Parsimmon.regexp(/[0-9]+/).map(str => Number.parseFloat(str)),
+    string: q => Parsimmon.regexp(/"(.*)"/, 1),
+    identifier: q => Parsimmon.regexp(/[a-zA-Z][\w_-]+/),
+    binaryPlusMinus: q => Parsimmon.regexp(/\+|-/).map(str => str as BinaryOp),
+    binaryCompareOp: q => Parsimmon.regexp(/>=|<=|>|<|=/).map(str => str as BinaryOp),
+
+    // Field parsing.
+    variableField: q => q.identifier.map(Fields.variable),
+    numberField: q => q.number.map(val => Fields.literal('number', val)),
+    stringField: q => q.string.map(val => Fields.literal('string', val)),
+    atomField: q => Parsimmon.alt(q.parensField, q.variableField, q.numberField, q.stringField),
+    binaryPlusMinusField: q => Parsimmon.seqMap(q.atomField, Parsimmon.seq(Parsimmon.optWhitespace, q.binaryPlusMinus, Parsimmon.optWhitespace, q.atomField).many(),
+        (first, rest) => {
+            if (rest.length == 0) return first;
+
+            let node = Fields.binaryOp(first, rest[0][1], rest[0][3]);
+            for (let index = 1; index < rest.length; index++) {
+                node = Fields.binaryOp(node, rest[index][1], rest[index][3]);
+            }
+            return node;
+        }),
+    binaryOpField: q => Parsimmon.seqMap(q.binaryPlusMinusField, Parsimmon.seq(Parsimmon.optWhitespace, q.binaryCompareOp, Parsimmon.optWhitespace, q.binaryPlusMinusField).many(),
+        (first, rest) => {
+            if (rest.length == 0) return first;
+
+            let node = Fields.binaryOp(first, rest[0][1], rest[0][3]);
+            for (let index = 1; index < rest.length; index++) {
+                node = Fields.binaryOp(node, rest[index][1], rest[index][3]);
+            }
+            return node;
+        }),
+    parensField: q => Parsimmon.seqMap(Parsimmon.oneOf("("), Parsimmon.optWhitespace, q.field, Parsimmon.optWhitespace, Parsimmon.oneOf(")"), (_1, _2, field, _3, _4) => field),
+    field: q => q.binaryOpField,
+    explicitNamedField: q => Parsimmon.seqMap(q.field, Parsimmon.whitespace, Parsimmon.regexp(/AS/i), Parsimmon.whitespace, q.identifier,
+        (field, _1, _2, _3, ident) => Fields.named(ident, field)),
+    namedField: q => Parsimmon.alt<NamedField>(
+        q.variableField.map(field => Fields.named(field.name, field)),
+        q.explicitNamedField
+    ),
+});
 
 /**
  * Attempt to parse a query from the given query text, returning a string error
--- src/test/query.test.ts
+++ src/test/query.test.ts
@@ -1,5 +1,74 @@
-import { parseQuery } from "../query";
+import { QUERY_LANGUAGE, parseQuery, QueryType, BinaryOpField, Fields, NamedField } from "../query";
+import { Success, Failure, Result } from "parsimmon";
 
-test("Parse Query", () => {
-    expect(parseQuery("list time-played from #game")).toBe("Not implemented");
+test("Parse Query Type", () => {
+    let unknown = QUERY_LANGUAGE.queryType.parse("vehicle");
+    expect(unknown.status).toBe(false);
+
+    let list = QUERY_LANGUAGE.queryType.parse("list") as Success<QueryType>;
+    expect(list.status).toBe(true);
+    expect(list.value).toBe('list');
+
+    let listUpper = QUERY_LANGUAGE.queryType.parse("lIsT") as Success<QueryType>;
+    expect(listUpper.status).toBe(true);
+    expect(listUpper.value).toBe('list');
+});
+
+test("Parse Identifier", () => {
+    let ident = QUERY_LANGUAGE.identifier.parse("lma0") as Success<string>;
+    expect(ident.status).toBe(true);
+    expect(ident.value).toBe("lma0");
+
+    let badIdent = QUERY_LANGUAGE.identifier.parse("0no");
+    expect(badIdent.status).toBe(false);
+});
+
+test("Parse Number Literal", () => {
+    expect(QUERY_LANGUAGE.number.parse("0no").status).toBe(false);
+
+    let res = QUERY_LANGUAGE.number.parse("123") as Success<number>;
+    expect(res.status).toBe(true);
+    expect(res.value).toBe(123);
+});
+
+test("Parse String Literal", () => {
+    let badResult = QUERY_LANGUAGE.string.parse("this won't work, no quotes");
+    expect(badResult.status).toBe(false);
+
+    let goodResult = QUERY_LANGUAGE.string.parse("\"hello\"") as Success<string>;
+    expect(goodResult.status).toBe(true);
+    expect(goodResult.value).toBe("hello");
+});
+
+test("Parse Simple Binary", () => {
+    let result = QUERY_LANGUAGE.binaryOpField.parse("16 + \"what\"") as Success<BinaryOpField>;
+    expect(result.status).toBe(true);
+    expect(result.value).toEqual(Fields.binaryOp(Fields.literal('number', 16), '+', Fields.literal('string', "what")));
+});
+
+test("Parse Parenthesis", () => {
+    let result = QUERY_LANGUAGE.field.parse("(16 - 4) - 8") as Success<BinaryOpField>;
+    expect(result.status).toBe(true);
+    expect(result.value).toEqual(Fields.binaryOp(Fields.binaryOp(Fields.literal('number', 16), '-', Fields.literal('number', 4)), '-', Fields.literal('number', 8)));
+});
+
+test("Order of Operations", () => {
+    let result = QUERY_LANGUAGE.field.parse("14 + 6 >= 19 - 2") as Success<BinaryOpField>;
+    console.log(result);
+    expect(result.status).toBe(true);
+    expect(result.value).toEqual(Fields.binaryOp(
+        Fields.binaryOp(Fields.literal('number', 14), '+', Fields.literal('number', 6)),
+        '>=',
+        Fields.binaryOp(Fields.literal('number', 19), '-', Fields.literal('number', 2)),
+    ));
+});
+
+test("Named Fields", () => {
+    let simple = QUERY_LANGUAGE.namedField.parse("time-played") as Success<NamedField>;
+    expect(simple.status).toBe(true);
+    expect(simple.value).toEqual(Fields.named("time-played", Fields.variable("time-played")));
+
+    let complex = QUERY_LANGUAGE.namedField.parse("(time-played + 4) as something") as Success<NamedField>;
+    expect(complex.status).toBe(true);
+    expect(complex.value).toEqual(Fields.named("something", Fields.binaryOp(Fields.variable("time-played"), '+', Fields.literal('number', 4))));
 });
