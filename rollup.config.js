f3384a743ed81b38d57bb63fee41126c0d927cf9
Davide Aversa <thek3nger@gmail.com>
2021-04-27 19:46:58 +0200

    Update rollup and package.json to the latest "standard"

--- rollup.config.js
+++ rollup.config.js
@@ -2,11 +2,14 @@ import typescript from '@rollup/plugin-typescript';
 import {nodeResolve} from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 
+const isProd = process.env.BUILD === "production";
+
 export default {
   input: 'src/main.ts',
   output: {
     dir: 'build',
     sourcemap: 'inline',
+    sourcemapExcludeSources: isProd,
     format: 'cjs',
     exports: 'default'
   },
