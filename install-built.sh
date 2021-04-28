a8803ec0c4ab084559c1ec7f2d1f93e9db0f44e4
Dave Slusher <dave@pop-os.localdomain>
2021-04-28 16:15:32 -0400

    Adding in quotes because $TARGET might have spaces

--- install-built.sh
+++ install-built.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 VAULT="$1"
 TARGET="$VAULT/.obsidian/plugins/dataview/"
-mkdir -p $TARGET
+mkdir -p "$TARGET"
 cp -f build/main.js styles.css manifest.json "$TARGET"
 echo Installed plugin files to "$TARGET"
