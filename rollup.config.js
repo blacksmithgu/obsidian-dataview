5ed6799c19abeb930050bb5b00572a8d83bf0db0
Michael Brenan <blacksmithgu@gmail.com>
2021-09-08 00:02:10 -0500

    Fix sourcemaps, fix build hangs, format rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,84 +1,83 @@
-import typescript from '@rollup/plugin-typescript';
-import nodeResolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import webWorker from 'rollup-plugin-web-worker-loader';
-import copy from 'rollup-plugin-copy';
-import ttypescript from 'ttypescript';
-import typescript2 from 'rollup-plugin-typescript2';
+import nodeResolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import webWorker from "rollup-plugin-web-worker-loader";
+import copy from "rollup-plugin-copy";
+import ttypescript from "ttypescript";
+import typescript2 from "rollup-plugin-typescript2";
 
 const LIBRARY_CONFIG = {
-  input: 'src/index.ts',
-  output: {
-    dir: 'lib',
-    sourcemap: true,
-    format: 'cjs',
-  },
-  external: ['obsidian'],
-  plugins: [
-    typescript2({
-      tsconfig: 'tsconfig-lib.json',
-      typescript: ttypescript
-    }),
-    nodeResolve({ browser: true }),
-    commonjs(),
-    webWorker({ inline: true, forceInline: true, targetPlatform: 'browser' }),
-  ],
-  onwarn: (warning, warn) => {
-    // Sorry rollup, but we're using eval...
-    if (/Use of eval is strongly discouraged/.test(warning.message)) return;
-    warn(warning);
-  }
+    input: "src/index.ts",
+    output: {
+        dir: "lib",
+        sourcemap: true,
+        format: "cjs",
+    },
+    external: ["obsidian"],
+    plugins: [
+        typescript2({
+            tsconfig: "tsconfig-lib.json",
+            typescript: ttypescript,
+        }),
+        nodeResolve({ browser: true }),
+        commonjs(),
+        webWorker({ inline: true, forceInline: true, targetPlatform: "browser" }),
+    ],
+    onwarn: (warning, warn) => {
+        // Sorry rollup, but we're using eval...
+        if (/Use of eval is strongly discouraged/.test(warning.message)) return;
+        warn(warning);
+    },
 };
 
 const PROD_PLUGIN_CONFIG = {
-  input: 'src/main.ts',
-  output: {
-    dir: 'build',
-    sourcemap: 'inline',
-    sourcemapExcludeSources: true,
-    format: 'cjs',
-    exports: 'default'
-  },
-  external: ['obsidian'],
-  plugins: [
-    nodeResolve({ browser: true }),
-    commonjs(),
-    webWorker({ inline: true, forceInline: true, targetPlatform: 'browser' }),
-    typescript()
-  ],
-  onwarn: (warning, warn) => {
-    // Sorry rollup, but we're using eval...
-    if (/Use of eval is strongly discouraged/.test(warning.message)) return;
-    warn(warning);
-  }
+    input: "src/main.ts",
+    output: {
+        dir: "build",
+        sourcemap: "inline",
+        sourcemapExcludeSources: true,
+        format: "cjs",
+        exports: "default",
+    },
+    external: ["obsidian"],
+    plugins: [
+        nodeResolve({ browser: true }),
+        commonjs(),
+        webWorker({ inline: true, forceInline: true, targetPlatform: "browser" }),
+        typescript2({ tsconfig: "tsconfig.json" }),
+    ],
+    onwarn: (warning, warn) => {
+        // Sorry rollup, but we're using eval...
+        if (/Use of eval is strongly discouraged/.test(warning.message)) return;
+        warn(warning);
+    },
 };
 
 const DEV_PLUGIN_CONFIG = {
-  input: 'src/main.ts',
-  output: {
-    dir: 'test-vault/.obsidian/plugins/dataview',
-    format: 'cjs',
-    sourcemap: 'inline',
-    exports: 'default'
-  },
-  external: ['obsidian'],
-  plugins: [
-    nodeResolve({ browser: true }),
-    commonjs(),
-    webWorker({ inline: true, forceInline: true, targetPlatform: 'browser' }),
-    typescript(),
-    copy({
-        targets: [
-            {src: 'manifest.json', dest: 'test-vault/.obsidian/plugins/dataview/'},
-            {src: 'styles.css', dest: 'test-vault/.obsidian/plugins/dataview/'}
-        ]
-    })
-  ],
-  onwarn: (warning, warn) => {
-    // Sorry rollup, but we're using eval...
-    if (/Use of eval is strongly discouraged/.test(warning.message)) return;
-    warn(warning);
-  }
+    input: "src/main.ts",
+    output: {
+        dir: "test-vault/.obsidian/plugins/dataview",
+        format: "cjs",
+        sourcemap: "inline",
+        exports: "default",
+    },
+    external: ["obsidian"],
+    plugins: [
+        nodeResolve({ browser: true }),
+        commonjs(),
+        webWorker({ inline: true, forceInline: true, targetPlatform: "browser" }),
+        typescript2({ tsconfig: "tsconfig.json" }),
+        copy({
+            targets: [
+                { src: "manifest.json", dest: "test-vault/.obsidian/plugins/dataview/" },
+                { src: "styles.css", dest: "test-vault/.obsidian/plugins/dataview/" },
+            ],
+        }),
+    ],
+    onwarn: (warning, warn) => {
+        // Sorry rollup, but we're using eval...
+        if (/Use of eval is strongly discouraged/.test(warning.message)) return;
+        warn(warning);
+    },
 };
 
 let configs = [];
