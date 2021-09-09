c10b31c1b02e02f40dd2cfb2175ccfd3b868a981
Michael Brenan <blacksmithgu@gmail.com>
2021-09-09 01:37:27 -0500

    Hotfix bad date web-worker transfer due to version bump

--- src/data/value.ts
+++ src/data/value.ts
@@ -1,4 +1,4 @@
-import { DateTime, Duration } from "luxon";
+import { DateTime, DateTimeJSOptions, Duration } from "luxon";
 import { DEFAULT_QUERY_SETTINGS, QuerySettings } from "settings";
 import { getFileTitle } from "util/normalize";
 
@@ -529,7 +529,7 @@ export type TransferableValue =
     | boolean
     | Array<any>
     | Record<string, any>
-    | { "___transfer-type": "date" | "duration" | "link" | "task"; value: Record<string, any> };
+    | { "___transfer-type": "date" | "duration" | "link" | "task"; value: Record<string, any>, options?: Record<string, any> }
 
 export namespace TransferableValues {
     /** Convert a literal value to a serializer-friendly transferable value. Does not work for all types. */
@@ -544,7 +544,7 @@ export namespace TransferableValues {
             case "boolean":
                 return wrapped.value;
             case "date":
-                return { "___transfer-type": "date", value: wrapped.value.toObject({ includeConfig: true }) };
+                return { "___transfer-type": "date", value: wrapped.value.toObject(), options: { zone: wrapped.value.zoneName } };
             case "duration":
                 return { "___transfer-type": "duration", value: wrapped.value.toObject() };
             case "array":
@@ -572,7 +572,7 @@ export namespace TransferableValues {
             if ("___transfer-type" in transferable) {
                 switch (transferable["___transfer-type"]) {
                     case "date":
-                        return DateTime.fromObject(value(transferable.value) as DataObject);
+                        return DateTime.fromObject(value(transferable.value) as DataObject, value(transferable.options || {}) as DateTimeJSOptions);
                     case "duration":
                         return Duration.fromObject(value(transferable.value) as DataObject);
                     case "link":
