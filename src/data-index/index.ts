1c447c9282e9d9c13fda61b3938993fb8a40c849
☙◦ The Tablet ❀ GamerGirlandCo ◦❧ <c.negrissa.the.angel@gmail.com>
2023-04-27 18:21:54 -0400

    move callback from constructor and into initialize

--- src/data-index/index.ts
+++ src/data-index/index.ts
@@ -82,13 +82,6 @@ export class FullIndex extends Component {
         /** this is needed because though canvas files will also fire the `modify` event,
             dataview can't already pick up on that. so we need to add the hook manually for canvases.
          */
-        this.registerEvent(
-            this.vault.on("modify", async file => {
-                if (file instanceof TFile && PathFilters.canvas(file.path)) {
-                    await this.reload(file);
-                }
-            })
-        );
     }
 
     /** Trigger a metadata event on the metadata cache. */
@@ -126,6 +119,13 @@ export class FullIndex extends Component {
             })
         );
 
+        this.registerEvent(
+            this.vault.on("modify", async file => {
+                if (file instanceof TFile && PathFilters.canvas(file.path)) {
+                    await this.reload(file);
+                }
+            })
+        );
         // Asynchronously initialize actual content in the background.
         this._initialize(this.vault.getFiles().filter(a => a.extension === "md" || a.extension === "canvas"));
     }
