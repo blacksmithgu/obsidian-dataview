3ad4e96abc7e5438b911b906b7c89224f8dfd8cf
Ryota Ushio <72342591+RyotaUshio@users.noreply.github.com>
2023-10-29 15:22:17 +0900

    Modify `renderCompactMarkdown()` so that it causes no flickering/scrolling in Live Preview (#2131)
    * Revert 7a2b6a1d but only for src/ui/render.ts
    
    * Fix the incorrect cursor overlapping check
    
    * Squashed commit of the following:
    
    commit f4e0bddf5f372db8afe4951e5f4246a1b840f5c4
    Author: RyotaUshio <ushio@ms.k.u-tokyo.ac.jp>
    Date:   Sat Oct 14 06:37:10 2023 +0900
    
        Fix typo
    
    commit 43f3be46e4e7d32195db18abc87aa6ce55f5285b
    Author: RyotaUshio <ushio@ms.k.u-tokyo.ac.jp>
    Date:   Sat Oct 14 06:29:01 2023 +0900
    
        Fix formatting
    
    commit 74e45e434766df822af06c33173a2f54a653b509
    Author: RyotaUshio <ushio@ms.k.u-tokyo.ac.jp>
    Date:   Sat Oct 14 06:28:32 2023 +0900
    
        Fix the starting position of an inline field could not obtained correctly after a document change
    
    commit 7570941c80501bb3d87bf9a4ff9fc6364f69b730
    Author: RyotaUshio <ushio@ms.k.u-tokyo.ac.jp>
    Date:   Sat Oct 14 06:07:03 2023 +0900
    
        Fix formatting
    
    commit ba7a0a5ea027b2c0416620050f8c5aaaa22d5e3f
    Author: RyotaUshio <ushio@ms.k.u-tokyo.ac.jp>
    Date:   Sat Oct 14 06:04:27 2023 +0900
    
        Fix the previous commit works only for the first line
    
    commit 08bcd329f30c2f5557b0722cd63a550b606cc241
    Author: RyotaUshio <ushio@ms.k.u-tokyo.ac.jp>
    Date:   Sat Oct 14 05:54:44 2023 +0900
    
        Resolve #2101: add a click event handler to inline field widgets in live preview
    
    * Squashed commit of the following:
    
    commit 21591ff9447bb757d10ec8ac300579bc19101058
    Author: RyotaUshio <ushio@ms.k.u-tokyo.ac.jp>
    Date:   Sat Oct 14 07:41:40 2023 +0900
    
        Fix #2107: Prevent inline fields inside a code block from being rendered in live preview
    
    * Squashed commit of the following:
    
    commit f787431d35283717aeb3380bbb229e1cef13cb5a
    Author: RyotaUshio <ushio@ms.k.u-tokyo.ac.jp>
    Date:   Sun Oct 29 00:41:52 2023 +0900
    
        fix formatting
    
    commit 7205a701b9f2ca6a214c0fb8156b9a89c9a20237
    Author: RyotaUshio <ushio@ms.k.u-tokyo.ac.jp>
    Date:   Sun Oct 29 00:28:24 2023 +0900
    
        separate the inline fields rendering option into one for reading view & one for live preview
    
    * fix the auto-scrolling/flickering issue of inline fields (#2128) and dv.paragraph not working for lists (#2099)
    
    * fix formatting
    
    ---------
    
    Co-authored-by: RyotaUshio <ushio@ms.k.u-tokyo.ac.jp>

--- src/ui/render.ts
+++ src/ui/render.ts
@@ -18,6 +18,33 @@ export async function renderCompactMarkdown(
     let paragraph = tmpContainer.querySelector(":scope > p");
     if (tmpContainer.childNodes.length == 1 && paragraph) {
         container.replaceChildren(...paragraph.childNodes);
+    } else {
+        /**
+         * In most cases, the condition above will be true.
+         * However, it is not always true, for example:
+         * ```dataviewjs
+         * dv.paragraph(`
+         * - list item 1
+         * - list item 2
+         *
+         * 1. list item 3
+         * 2. list item 4
+         * `)
+         * ```
+         * MarkdownRenderer.renderMarkdown will render it as:
+         * <span>
+         *   <ul>
+         *     <li>list item 1</li>
+         *     <li>list item 2</li>
+         *   </ul>
+         *   <ol>
+         *     <li>list item 3</li>
+         *     <li>list item 4</li>
+         *   </ol>
+         * </span>
+         * Notice that there is no <p> tag.
+         */
+        container.replaceChildren(...tmpContainer.childNodes);
     }
 
     tmpContainer.remove();
