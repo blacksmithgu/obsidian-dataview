5e9fc3bd37b15d3062800d2651549663edfa3ec1
Michael Naumov <mnaoumov@gmail.com>
2024-05-13 22:53:59 -0600

    Configure CodeMirror to treat dataviewjs as javascript and revert on unload

--- src/main.ts
+++ src/main.ts
@@ -161,6 +161,9 @@ export default class DataviewPlugin extends Plugin {
                 });
             })
         );
+
+        window.CodeMirror.defineMode("dataviewjs", config => window.CodeMirror.getMode(config, "javascript"));
+        this.register(() => window.CodeMirror.defineMode("dataviewjs", config => window.CodeMirror.getMode(config, "null")));
     }
 
     private debouncedRefresh: () => void = () => null;
