22a7160e5cb1d7e499687d345eaf31010a9944dd
holroy <holroy@users.noreply.github.com>
2025-02-25 21:08:58 +0100

    Added the same changes also to reading mode
    I somehow forgot to apply the changes to reading mode

--- src/ui/views/inline-field-live-preview.ts
+++ src/ui/views/inline-field-live-preview.ts
@@ -260,6 +260,10 @@ class InlineFieldWidget extends WidgetType {
         } else {
             const value = renderContainer.createSpan({
                 cls: ["dataview", "inline-field-standalone-value"],
+                attr: {
+                    "data-dv-key": this.field.key,
+                    "data-dv-norm-key": canonicalizeVarName(this.field.key),
+                },
             });
             renderValue(
                 this.app,
