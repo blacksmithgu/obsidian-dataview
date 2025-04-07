77ab745aee787d519642a87ed8f68be12fdc4b0d
holroy <holroy@users.noreply.github.com>
2025-04-07 21:05:03 +0200

    Bug fix: Renders all elements in an inline field in Live Preview
    Related to #2557, I tracked that bug down to #2416 which introduced code rendering on the last element within an inline field, when in _Live preview_. Now we loop on all elements, and render them all.

--- src/ui/render.ts
+++ src/ui/render.ts
@@ -42,7 +42,9 @@ async function renderCompactMarkdownForInlineFieldLivePreview(
     await MarkdownRenderer.render(app, markdown, tmpContainer, sourcePath, component);
     let paragraph = tmpContainer.querySelector(":scope > p");
     if (tmpContainer.childNodes.length == 1 && paragraph) {
-        container.appendChild(paragraph.childNodes.item(paragraph.childNodes.length - 1));
+        while (paragraph.firstChild) {
+            container.appendChild(paragraph.firstChild);
+        }
     } else {
         container.replaceChildren(...tmpContainer.childNodes);
     }
